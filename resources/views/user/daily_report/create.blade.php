e42c8d3ad99b1b5f1cc3bb494aee0ba567176fa2
vagrant <vagrant@homestead>
2019-11-12 06:37:26 +0000

    forms in the blades are changed from HTML tags to Laravel Facade form

--- resources/views/user/daily_report/create.blade.php
+++ resources/views/user/daily_report/create.blade.php
@@ -5,20 +5,20 @@
 <div class="main-wrap">
   <div class="container">
     {{ Form::open(['route' => 'daily_report.create']) }}
-      <input class="form-control" name="user_id" type="hidden">
+      {{ Form::hidden('user_id', null, ['class' => 'form-control']) }}
       <div class="form-group form-size-small @if(!empty($errors->first('reporting_time'))) has-error @endif">
-        <input class="form-control" name="reporting_time" type="date">
+        {{ Form::input('date', 'reporting_time', null, ['class' => 'form-control']) }}
         <span class="help-block">{{ $errors->first('reporting_time') }}</span>
       </div>
       <div class="form-group @if(!empty($errors->first('title'))) has-error @endif">
-        <input class="form-control" placeholder="Title" name="title" type="text">
+        {{ Form::input('text', 'title', null, ['class' => 'form-control', 'placeholder' => 'Title']) }}
         <span class="help-block">{{ $errors->first('title') }}</span>
       </div>
       <div class="form-group @if(!empty($errors->first('content'))) has-error @endif">
-        <textarea class="form-control" placeholder="Content" name="content" cols="50" rows="10"></textarea>
+        {{ Form::textarea('content', null, ['class' => 'form-control', 'placeholder' => 'Content', 'cols' => 50, 'rows' => 10]) }}
         <span class="help-block">{{ $errors->first('content') }}</span>
       </div>
-      <button type="submit" class="btn btn-success pull-right">Add</button>
+      {{ Form::submit('Add', ['class' => 'btn btn-success pull-right']) }}
     {{ Form::close() }}
   </div>
 </div>
