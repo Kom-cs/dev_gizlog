9367d4d61c804929fadc21ed66f7d61ddbca6325
vagrant <vagrant@homestead>
2019-11-15 09:35:11 +0000

    removed unnecessary var definition

--- database/factories/DailyReportsFactory.php
+++ database/factories/DailyReportsFactory.php
@@ -4,10 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\DailyReport::class, function (Faker $faker) {
     return [
-        'user_id' => $faker->numberBetween($min = 1, $max = 50),
-        'title' => $faker->realText($maxNbChars = 30, $indexSize = 2),
-        'content'=>$faker->realText($maxNbChars = 50, $indexSize = 2),
-        'reporting_time' => $faker->dateTime($max = 'now', date_default_timezone_get()),
-        'created_at' => $faker->dateTime($max = 'now', date_default_timezone_get()),
+        'user_id' => $faker->numberBetween(1, 50),
+        'title' => $faker->realText(30, 2),
+        'content'=>$faker->realText(50, 2),
+        'reporting_time' => $faker->dateTime('now', date_default_timezone_get()),
+        'created_at' => $faker->dateTime('now', date_default_timezone_get()),
     ];
 });
