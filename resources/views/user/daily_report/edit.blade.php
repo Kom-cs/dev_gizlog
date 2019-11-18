76c396087cd5e75b11f156807749573dbf19d958
vagrant <vagrant@homestead>
2019-11-18 04:34:57 +0000

    data-type of the edit form for the title defined by a facade method

--- resources/views/user/daily_report/edit.blade.php
+++ resources/views/user/daily_report/edit.blade.php
@@ -10,7 +10,7 @@
       <span class="help-block">{{ $errors->first('reporting_time') }}</span>
       </div>
       <div class="form-group @if($errors->has('title')) has-error @endif">
-        {{ Form::input('text', 'title', $dailyReport->title, ['class' => 'form-control', 'placeholder' => 'Title']) }}
+        {{ Form::text('title', $dailyReport->title, ['class' => 'form-control', 'placeholder' => 'Title']) }}
       <span class="help-block">{{ $errors->first('title') }}</span>
       </div>
       <div class="form-group @if($errors->has('content')) has-error @endif">
